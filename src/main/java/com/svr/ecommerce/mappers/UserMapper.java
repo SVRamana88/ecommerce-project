d019ac42982b36eeda4615583f38a3d21f1efad4
Venkata Ramana Seelam <venkata.devjourney@gmail.com>
2026-02-15 16:16:33 +0530

    use serilization jsonFormattr

--- src/main/java/com/svr/ecommerce/mappers/UserMapper.java
+++ src/main/java/com/svr/ecommerce/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.svr.ecommerce.mappers;
 import com.svr.ecommerce.dtos.UserDto;
 import com.svr.ecommerce.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
