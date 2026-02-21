3b48684906e54505ab00077dbf0f3a5a586ad112
Venkata Ramana Seelam <venkata.devjourney@gmail.com>
2026-02-21 18:53:26 +0530

    add updat user api

--- src/main/java/com/svr/ecommerce/controllers/UserController.java
+++ src/main/java/com/svr/ecommerce/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.svr.ecommerce.controllers;
 
 import com.svr.ecommerce.dtos.RegisterUserRequest;
+import com.svr.ecommerce.dtos.UpdateUserRequest;
 import com.svr.ecommerce.dtos.UserDto;
 import com.svr.ecommerce.entities.User;
 import com.svr.ecommerce.mappers.UserMapper;
@@ -55,5 +56,18 @@ public class UserController {
         var userDto = userMapper.toDto(user);
         var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
         return ResponseEntity.created(uri).body(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable(name = "id") Long id,
+            @RequestBody UpdateUserRequest request
+            ) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) return  ResponseEntity.notFound().build();
+        userMapper.update(request, user);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 }
--- src/main/java/com/svr/ecommerce/mappers/UserMapper.java
+++ src/main/java/com/svr/ecommerce/mappers/UserMapper.java
@@ -1,10 +1,12 @@
 package com.svr.ecommerce.mappers;
 
 import com.svr.ecommerce.dtos.RegisterUserRequest;
+import com.svr.ecommerce.dtos.UpdateUserRequest;
 import com.svr.ecommerce.dtos.UserDto;
 import com.svr.ecommerce.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -12,4 +14,6 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     User toEntity(RegisterUserRequest request);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
